40039e2244dd415bda24b8875d0f09631488300d
gauravpks <66349530+gauravpks@users.noreply.github.com>
2020-09-28 16:48:05 +0530

    Update data_preprocessing_tools.py
    File Location Update

--- Part 1 - Data Preprocessing/data_preprocessing_tools.py
+++ Part 1 - Data Preprocessing/data_preprocessing_tools.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('https://raw.githubusercontent.com/gauravpks/ml-repo/master/Part%201%20-%20Data%20Preprocessing/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
